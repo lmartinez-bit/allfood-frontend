dbda5d754409a427d9bf07c74aa47eedd505e839
Luis Gamarra <luisgamarrarosado@gmail.com>
2024-09-10 00:44:17 -0500

    Inicio de proyecto Allfood Inicio, Acerca de Nosotros y Contactenos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'aboutus', component: AboutusComponent},
+    { path: 'contactus', component: ContactusComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' } // Ruta predeterminada
+  ];
+
