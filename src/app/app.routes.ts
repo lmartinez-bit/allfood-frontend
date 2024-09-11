e332e414eaa2df3856a5a7624b7fae41924c85df
Luis Gamarra <luisgamarrarosado@gmail.com>
2024-09-11 08:31:12 -0500

    Creación del proyecto, páginas Home, Aboutus y Contactus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+    { path: 'aboutus', component: AboutusComponent},
+    { path: 'contactus', component: ContactusComponent},
+    { path: '**', pathMatch: 'full', redirectTo: 'home' } // Ruta predeterminada
+];
